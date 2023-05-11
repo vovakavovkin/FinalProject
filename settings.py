8b7c7500b41a9c5ebc5d0447b704425a3b39b1f3
vovakavovkin <vovakavovkin@yandex.ru>
2023-05-11 11:55:15 +0300

    add files

---
+++ settings.py
@@ -0,0 +1,25 @@
+class Settings:
+    chrome_driver = 'C:\chromedriver/chromedriver.exe'
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'vovakavovkin@yandex.ru'
+    valid_password = '17Fduecnf'
+    valid_email_for_reg = 'dertyuiref@mail.ru'
+    invalid_email = 'vovavova@gmail.com'
+    invalid_password = '123654789'
+    empty_email = ''
+    empty_password = ''
+    menu_of_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name_of_user = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    f_name = 'Иван'
+    l_name = 'Иванов'
+    f_name_ = '-'
+    l_name_ = '-'
+    russian_generate_string = 'и'
+    latin_generate_string = 'ssttww'
+    chinese_chars = '我不会说汉语请做不辣的怎么样你会说英语'
+    special_chars = '!@#$$%^&*()_+_+=//{}'
+    numbers = 987
+    empty = ''
+    passw1 = 'qQ654321'
+    passw2 = 'Qw12345678Wq543'
+    passw3 = 'Qa12345678Er12345978'
